35ce2cd26273085581052ebd9286ae7c0c13604d
whenipush <sunsetbb@yahoo.com>
2022-07-07 16:08:13 +0300

    admin panel custom

--- codeshop/shop/admin.py
+++ codeshop/shop/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Category, Product
 
-# Register your models here.
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['category_name', 'slug']
+    prepopulated_fields = {'slug': ('category_name', )}
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug', 'price', 'available', 'created', 'uploaded']
+    list_filter = ['available', 'created', 'uploaded']
+    list_editable = ['price', 'available']
+    prepopulated_fields = {'slug': ('name', )}
